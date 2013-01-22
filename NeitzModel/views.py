1c3a144d4ce31a8cdae38795ed1b4ea74274a466
Brian Schmidt <brian.patrick.schmidt@gmail.com>
2013-01-21 17:25:48 -0800

    added first line

--- NeitzModel/views.py
+++ NeitzModel/views.py
@@ -1,11 +1,13 @@
 from django.template import RequestContext, loader
 from django.http import HttpResponse
+from django.utils import simplejson
 
 from eschaton.eschaton import eye
 schemEye = eye.eyeModel.SchematicEye().returnOSLOdata()
 
+array = schemEye['farPeriph']['40deg'].tolist()
 
 def index(request):
     t = loader.get_template('index.html')
-    c = RequestContext(request,{'opticDict':schemEye,})
+    c = RequestContext(request,{'opticDict':schemEye,'MTF':array})
     return HttpResponse(t.render(c))
