4e785e5ceb0157621cf1f270fc0b8e56bb3133d7
Brian Schmidt <brian.patrick.schmidt@gmail.com>
2013-01-21 17:38:51 -0800

    added second line

--- NeitzModel/views.py
+++ NeitzModel/views.py
@@ -5,9 +5,12 @@ from django.utils import simplejson
 from eschaton.eschaton import eye
 schemEye = eye.eyeModel.SchematicEye().returnOSLOdata()
 
-array = schemEye['farPeriph']['40deg'].tolist()
+opticsA = schemEye['farPeriph']['40deg'].tolist()
+opticsB = schemEye['farPeriph']['20deg'].tolist()
 
 def index(request):
     t = loader.get_template('index.html')
-    c = RequestContext(request,{'opticDict':schemEye,'MTF':array})
+    c = RequestContext(request,{'opticDict':schemEye,
+                                'MTF_A':opticsA,
+                                'MTF_B':opticsB,})
     return HttpResponse(t.render(c))
